f26d03b5a14f0ebb0bbfc1f4dd78585929cb1b62
André Luiz H. Kanashiro <alhkanashiro@gmail.com>
2021-05-09 10:21:36 -0300

    BarChart integration e DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,5 +1,33 @@
+import { Seller } from './seller';
+
 export type SaleSum = {
-  sellername: string;
+  sellerName: string;
   sum: number;
 };
 
+export type SaleSuccess = {
+  sellerName: string;
+  visited: number;
+  deals: number;
+};
+
+export type Sale = {
+  id: number;
+  visited: number;
+  deals: number;
+  amount: number;
+  date: string;
+  seller: Seller;
+};
+
+export type SalePage = {
+  content?: Sale[];
+  last: boolean;
+  totalPages: number;
+  totalElements: number;
+  first: boolean;
+  size?: number;
+  number: number;
+  numberOfElements?: number;
+  empty?: boolean;
+};
